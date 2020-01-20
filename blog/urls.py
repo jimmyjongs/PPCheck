0f51b8f722f5a122e0052f4d27f2931436454f3d
Jimmy Ong <jimmy@DESKTOP-B8BFHSP.home>
2020-01-19 16:50:56 -0800

    add password reset, pagination

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 
 # when using class based views, you cannot directly use the class
@@ -8,8 +8,11 @@ from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView,
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('user/<str:username>/', UserPostListView.as_view(), name='user-posts'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/new', PostCreateView.as_view(), name='post-create'),
     path('about/', views.about, name='blog-about'),
+
+
 ]
--- users/models.py
+++ users/models.py
@@ -10,8 +10,8 @@ class Profile(models.Model):
         return f'{self.user.username} Profile'
 
     # resize profile image
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
         img = Image.open(self.image.path)
 
         if img.height > 300 or img.width > 300:
